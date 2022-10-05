7e34c9469bed3120758f0ed21cc0b8b1f465a23d
Yied Pozi <yiedpozi@gmail.com>
2022-10-05 08:39:25 +0800

    fix: update copy

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    @if ($tasks->total() > 0)
+    @if ($tasks->total() > 1111110)
         <h3 class="text-2xl font-bold tracking-tighest">{{ __('To Do Lists') }}</h3>
         <span class="text-gray-400 text-sm font-medium mt-1 mb-8 block">{{ trans_choice('{0} There are no task done yet. Lets go!|{1} :count task done. Keep up the good work!|[2,*] :count tasks done. Keep up the good work!', $total_tasks_done) }}</span>
 
@@ -88,10 +88,10 @@
         <main class="mx-auto max-w-7xl p-2 sm:px-6 sm:py-8">
             <div class="sm:text-center lg:text-left">
                 <h1 class="text-4xl text-gray-900 font-bold tracking-tight sm:text-6xl">
-                    <span class="block xl:inline">Organize your</span>
-                    <span class="block text-pink-600 xl:inline">work and life</span>
+                    <span class="block xl:inline">Stay focused and</span>
+                    <span class="block text-pink-600 xl:inline">organized</span>
                 </h1>
-                <p class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">{!! __('Become focused, organized, and calm with :app_name. The world’s #1 task manager and to-do list app.', ['app_name' => '<span class="font-semibold">' . __( 'Laravel To Do List App' ) . '</span>']) !!}</p>
+                <p class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">{!! __('Become focused and organized with :app_name. Simple yet powerful to-do-list app for everyday use.', ['app_name' => '<span class="font-semibold">' . __( 'Laravel To Do List App' ) . '</span>']) !!}</p>
                 <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                     <div class="rounded-md shadow">
                         <a href="{{ route('task.create') }}" class="flex w-full items-center justify-center rounded-md border border-transparent bg-pink-600 px-8 py-3 text-base font-medium text-white hover:bg-pink-700 md:py-4 md:px-10 md:text-lg">{{ __('Get started') }}</a>
